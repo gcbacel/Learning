c6bec4fd87fdd1146687b9b45a2cba5ab3fa2ed2
gcbacel <55672618+gcbacel@users.noreply.github.com>
2021-03-26 13:11:30 -0700

    Update split_mp4video_in_parts.py

--- split_mp4video_in_parts.py
+++ split_mp4video_in_parts.py
@@ -8,7 +8,7 @@ from moviepy.editor import VideoFileClip
 
 # split a video in n parts by different duration (in seconds). last_block = True includes the final block of the video
 # Ex: split_video("video.mp4", 120, 240, 1200, last_block = True) splits video.mp4 in 3 parts from 120-240s, 240-1200s and 1200s-end of video
-def split_video(mp4_file, *args, last_block = False):
+def split_video(video_file, *args, last_block = False):
     video_splits = list(args)
     if last_block:
         video_size = VideoFileClip(full_video).duration
